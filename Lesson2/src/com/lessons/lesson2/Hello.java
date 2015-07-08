8cd5d129d313075327f0775f9587806994b26e28
nocs00 <nocs00@gmail.com>
2015-07-08 21:15:26 +0300

    Creating lesson2

---
+++ Lesson2/src/com/lessons/lesson2/Hello.java
@@ -0,0 +1,14 @@
+package com.lessons.lesson2;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test string");
+	}
+
+}
